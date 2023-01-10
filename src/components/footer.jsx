0df0c7e2ddc34a49d09bcc87637ccc6fc7852def
Manuel Alberto Gorrin <manuelalberto.gorrin@gmail.com>
2023-01-10 18:26:35 +0100

    fixing footer :)

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ function Footer(){
     return(
 
         <footer
-            className="relative bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+            className="relative bottom-0 left-0 z-20 w-full p-2 bg-gray-700 md:flex md:items-center md:justify-between md:p-3">
     <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/"
                                                                                         className="hover:underline">Manuel™</a>. All Rights Reserved.
     </span>
